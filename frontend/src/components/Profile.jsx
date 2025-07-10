fe02a096acf837507c4ddfcad42789cd597e2b4e
Shubham Patwal <shubhampatwal017@gmail.com>
2025-07-10 12:50:08 +0530

    update in appliedJobtable.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 import { Contact, Mail, Pen } from "lucide-react";
 import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
-import AppliedJobtable from "./AppliedJobtablejsx";
+import AppliedJobtable from "./AppliedJobtable.jsx";
 import UpdateProfileDialog from "./UpdateProfileDialog";
 import { useSelector } from "react-redux";
 import useGetAppliedJobs from "@/hooks/useGetAppliedJob";
