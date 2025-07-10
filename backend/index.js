334d55d2146828243afd9c7e4cd47112416f6d23
Shubham Patwal <shubhampatwal017@gmail.com>
2025-07-10 13:37:28 +0530

    change in index.js file

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["https://hire-bharat.vercel.app", "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
