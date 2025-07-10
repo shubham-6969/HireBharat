d01cb802cc780ede2d2b7a956a66319da3cb7d01
Shubham Patwal <shubhampatwal017@gmail.com>
2025-07-10 12:46:07 +0530

    fix: corrected import case for AppliedJobtable

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 import { Contact, Mail, Pen } from "lucide-react";
 import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
-import AppliedJobtable from "./AppliedJobtable";
+import AppliedJobtable from "./AppliedJobtablejsx";
 import UpdateProfileDialog from "./UpdateProfileDialog";
 import { useSelector } from "react-redux";
 import useGetAppliedJobs from "@/hooks/useGetAppliedJob";
