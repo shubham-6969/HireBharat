09181d49be508f1276201d4ad67da110d53165c8
Shubham Patwal <shubhampatwal017@gmail.com>
2025-07-12 11:54:47 +0530

    update in index.js file

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.use("/api/v1/application", applicationRoute);
 
 
 // server listening
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   connectDB();
   console.log(`Servr running on port: ${PORT}`);
