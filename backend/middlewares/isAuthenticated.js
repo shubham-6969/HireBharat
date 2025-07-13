f8439804e98ab83bc90b9e70c37025feb0675f62
Shubham Patwal <shubhampatwal017@gmail.com>
2025-07-13 09:17:24 +0530

    update in authenticationfile

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
   try {
-    console.log(" Cookies received:", req.cookies);
 
     const token = req.cookies.token;
     if(!token) {
